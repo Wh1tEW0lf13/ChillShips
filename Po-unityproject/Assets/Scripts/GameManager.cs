072684433f3a94f0abe8e8f62dfca0a650f69442
Marceli Błaszczyk <145374483+marseley@users.noreply.github.com>
2024-06-16 17:59:06 +0200

    Update GameManager.cs

--- Po-unityproject/Assets/Scripts/GameManager.cs
+++ Po-unityproject/Assets/Scripts/GameManager.cs
@@ -36,8 +36,8 @@ public class GameManager : MonoBehaviour
         asteroidKillCountRed = 0;
         asteroidKillCountBlue = 0;
         simulationTime = 0f;
-        ShipKiller.blueKillCount = 0;
-        ShipKiller.redKillCount = 0;
+        blueKillCount = 0;
+        redKillCount = 0;
 
     }
 
